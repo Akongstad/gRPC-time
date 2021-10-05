1967405ed9640e6162bd7c8af1aba0d77bacaf98
Akongstad <kongstad@outlook.com>
2021-10-05 12:11:17 +0200

    init

---
+++ Client/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"log"
+	"mod/Time"
+	"time"
+)
+
+const (
+	address = ":9000"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := Time.NewTimeServiceClient(conn)
+	log.Printf("My time is", time.Now())
+	time.Sleep(time.Second * 1)
+	clientMessage := "Client says: Give time pls"
+	log.Printf(clientMessage)
+	request := Time.Message{Body: clientMessage}
+	r, err := c.GetTime(context.Background(), &request)
+	if err != nil {
+		log.Fatalf("could not find get time: %v", err)
+	}
+	log.Printf("Server says: %s", r)
+}
---
+++ Server/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"log"
+	"mod/Time"
+	"net"
+	"time"
+)
+
+const (
+	port = ":9000"
+)
+
+type server struct {
+	Time.UnimplementedTimeServiceServer
+}
+
+func (s *server) GetMessage(ctx context.Context, r *Time.Message) (*Time.Message, error) {
+	log.Printf("Received msg %r", r.GetBody())
+	return &Time.Message{Body: time.Now().String()}, nil
+}
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Failed to listen port: %v", err)
+	}
+	s := server{}
+	grpcServer := grpc.NewServer()
+
+	Time.RegisterTimeServiceServer(grpcServer, &s)
+
+	log.Printf("server listening at %v", lis.Addr())
+	//fmt.Printf("server listening at %v", lis.Addr())
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed serve server: %v", err)
+	}
+}
